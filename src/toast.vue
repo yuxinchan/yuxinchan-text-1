f47484aae91e904e5e9a582f4186e4e18e4995c5
yuxinchan <837330534@qq.com>
2019-09-09 16:33:39 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -88,7 +88,12 @@
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg: rgba(0,0,0,0.75);
+    @keyframes fade-in {
+        0% {opacity: 0;}
+        100% {opacity: 1;}
+    }
     .toast {
+        animation: fade-in 1s;
         font-size: $font-size; line-height: 1.8; min-height: $toast-min-height;
         background: $toast-bg; color: #fff; position: fixed;
         display: flex; align-items: center; border-radius: 4px;
