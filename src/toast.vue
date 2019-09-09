003aa3ffbe614ab4c95c4b01ea608bb9059faeea
yuxinchan <837330534@qq.com>
2019-09-09 10:06:09 +0800

    添加 toast 和 plugin

---
+++ src/toast.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GuluToast'
+    }
+</script>
+
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0,0,0,0.75);
+    .toast {
+        font-size: $font-size;
+        line-height: 1.8;
+        height: $toast-height;
+        background: $toast-bg;
+        color: #fff;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex;
+        align-items: center;
+        border-radius: 4px;
+        box-shadow: 0px 0px 3px 0px rgba(0,0,0,0.5);
+        padding: 16px;
+    }
+</style>
