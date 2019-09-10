89e9a6af94a43f5442b8868e7124ac5954e604d9
yuxinchan <837330534@qq.com>
2019-09-10 11:41:04 +0800

    tabs 实现可切换

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tabs-item">
+    <div class="tabs-item" @click="xxx" :class="classes">
         <slot></slot>
     </div>
 </template>
@@ -7,10 +7,37 @@
 <script>
     export default {
         name: 'GuluTabsItem',
+        inject: ['eventBus'],
+        data() {
+            return {
+                active: false
+            }
+        },
         props: {
             disabled: {
                 type: Boolean,
                 default: false
+            },
+            name: {
+                type: String|Number,
+                required: true
+            }
+        },
+        computed: {
+            classes() {
+                return {
+                    active: this.active
+                }
+            }
+        },
+        created() {
+            this.eventBus.$on('update:selected', (name) => {
+                this.active = name === this.name;
+            })
+        },
+        methods: {
+            xxx() {
+                this.eventBus.$emit('update:selected',this.name)
             }
         }
     }
@@ -18,6 +45,11 @@
 
 <style lang="scss" scoped>
     .tabs-item {
-
+        padding: 0 1em;
+        flex-shrink: 0;
+        &.active {
+            background: #333;
+            color: #fff;
+        }
     }
 </style>
