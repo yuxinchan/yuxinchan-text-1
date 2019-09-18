d76d56679d8551ba10ce27e220d90ea43743ef48
yuxinchan <837330534@qq.com>
2019-09-18 12:20:35 +0800

    实现无缝切换

--- src/demo.vue
+++ src/demo.vue
@@ -44,6 +44,9 @@
         width: 100%;
         height: 350px;
         background: #ddd;
-        border: 1px solid #999;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 50px;
     }
 </style>
--- src/slides-item.vue
+++ src/slides-item.vue
@@ -37,14 +37,14 @@
         height: 100%;
     }
     .slide-enter-active, .slide-leave-active {
-        transition: all 1s;
+        transition: all .5s;
     }
     .slide-enter {
-        transform: translateX(100%);
+        transform: translateX(100%) scale(0.5);
         opacity: 0;
     }
     .slide-enter.reverse {
-        transform: translateX(-100%);
+        transform: translateX(-100%) scale(0.5);
         opacity: 0;
     }
     .slide-leave-to {
